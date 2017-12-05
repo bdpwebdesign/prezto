1b7fb396dfa2051bb5dbbacfdbf26afcd46b24d8
Brandon Peter <bdp@bdpwebdesign.com>
2017-12-04 22:20:35 -0500

    update prezto

---
+++ modules/colors/init.zsh
@@ -0,0 +1,18 @@
+# reset
+reset="[00m"
+# effects
+bold="[01m"
+faint="[02m"
+italic="[03m"
+underline="[04"
+blink="[05m"
+standout="[07m"
+# colors
+black="[30m"
+red="[31m"
+green="[32m"
+yellow="[33m"
+blue="[34m"
+magenta="[35m"
+cyan="[36m"
+white="[37m"
